ecad741991334bc8cae5bf63eeda1b0435f0b56d
Valentin <portenius@gmail.com>
2017-11-29 19:34:06 +0600

    Create basic model

--- app/src/main/java/com/example/user/gallows/GallowsModel.java
+++ app/src/main/java/com/example/user/gallows/GallowsModel.java
@@ -5,4 +5,21 @@ package com.example.user.gallows;
  */
 
 class GallowsModel {
+    private String secretWord;
+    private String guessedWord;
+
+
+
+    public GallowsModel() {
+        secretWord = "apple";
+    }
+
+
+    public String getSecretWord() {
+        return secretWord;
+    }
+
+    public String getGuessedWord() {
+        return guessedWord;
+    }
 }
